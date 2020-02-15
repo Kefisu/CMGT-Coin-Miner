93901e047a98dc7dfc48e711661bb957fddc7cb6
Kevin Jansen <kevitius99@gmail.com>
2020-02-15 22:08:16 +0100

    Dynamic second string

--- modules/hash.js
+++ modules/hash.js
@@ -3,6 +3,12 @@ module.exports = {
         return obj.blockchain.hash + obj.blockchain.data[0].from + obj.blockchain.data[0].to + obj.blockchain.data[0].amount + obj.blockchain.data[0].timestamp + obj.blockchain.timestamp + obj.blockchain.nonce;
     },
     createNewBlockString: (string, obj) => {
-        return string + obj.transactions[0].from + obj.transactions[0].to + obj.transactions[0].amount + obj.transactions[0].timestamp + obj.timestamp;
+        let transactionString = '';
+
+        for (let transaction of obj.transactions) {
+            transactionString += transaction.from + transaction.to + transaction.amount + transaction.timestamp
+        }
+
+        return string + transactionString + obj.timestamp;
     },
 }
